7e830c98385215250988fe205909a37c13da1b31
Igor Gabriel <dev.igorsousa@gmail.com>
2022-10-22 05:25:09 -0700

    first commit 22/10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,47 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('produtos', ProdutoController::class);
 
-Route::any('/any', function () {
-    return "Permite todo tipo de acesso HTTP.";
-});
-
-Route::match(['get', 'delete'], '/match', function () {
-    return "Permite apenas acessos definidos";
-});
-
-Route::get('/produto/{id}/{cat?}', function ($id, $cat = "limpeza") {
-    return "O id do produto é: ".$id."<br>"."E sua categoria é: ".$cat;
-});
-
-Route::redirect('/sobre', 'empresa');
-
-Route::view('/empresa', 'site/empresa');
-
-Route::get('/news', function() {
-    return view('news');
-})->name('noticias');
-
-Route::get('/novidades', function () {
-    return redirect()->route('noticias');
-});
-
-
-Route::group([
-    "prefix" => "admin",
-    "as" => "admin."
-], function () {
-    Route::get('dashboard', function() {
-        return "Dashboard";
-    })->name('dashboard');
-    Route::get('produtos', function() {
-        return "Produtos";
-    })->name('produtos');
-    Route::get('clientes', function() {
-        return "Clientes";
-    })->name('clientes');
-});
 
